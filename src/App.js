5a0d2baded28a237d3957dddea334973d5a17ae7
Kris Pritchard <krisrp@users.noreply.github.com>
2020-05-14 14:47:34 +1200

    Update code to latest version

--- src/App.js
+++ src/App.js
@@ -5,8 +5,63 @@ import './App.css'
 
 function MyComponent(props) {
   console.log(`These are props: ${props}`)
-  debugger
+  //debugger
   return (<h1>Hello from MyComponent: {props.name}</h1>)
+}
+
+class MyClassBasedComponent extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      counter: 0,
+      name: '',
+      age: '',
+      country: '',
+    }
+  }
+
+  // Remember to use Arrow Functions if you have problems with 'this' is not
+  // defined
+  increment = () => {
+    this.setState({
+      counter: this.state.counter + 1,
+    })
+  }
+
+  handleInput = (e) => {
+    console.log(`Input: ${e}`)
+    // Remember to set 'name' prop on your input fields.
+    this.setState({
+      [e.target.name]: e.target.value,
+    })
+  }
+
+  handleSubmit = (e) => {
+    e.preventDefault()
+    console.log(e)
+  }
+
+  render() {
+    // Remember to wrap your returns in a <div> if you have adjacent JSX
+    // elements.
+    return (
+      <div>
+        <h2>Counter: {this.state.counter}</h2>
+        <h2>Name: {this.state.name}</h2>
+        <h2>Age: {this.state.age}</h2>
+
+        <form onSubmit={this.handleSubmit}>
+          <input name="name" onChange={this.handleInput} placeholder="name" />
+          <input name="age" onChange={this.handleInput} placeholder="age" />
+          <input name="country" onChange={this.handleInput} placeholder="country" />
+          <button onClick={this.increment}>Increment</button>
+        </form>
+      </div>
+    )
+  }
+
+}
+
 
 
 function App() {
@@ -17,6 +72,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+          <MyClassBasedComponent />
           <MyComponent name="Blah" somemath={1 + 3} />
           <MyComponent name="Blah">These two lines are equivalent</MyComponent>
         <a
